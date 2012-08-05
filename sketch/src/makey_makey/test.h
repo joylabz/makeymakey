889310bfff0889469e020d9547864d5993200363
Tyler Williams <williams.tyler@gmail.com>
2012-08-05 17:35:43 -0400

    latest

--- sketch/src/makey_makey/test.h
+++ sketch/src/makey_makey/test.h
@@ -1,8 +1,8 @@
-#include "Arduino.h"
+#ifndef TEST_H
+#define TEST_H
 
-#define DEBUG_WAIT_TIME_MS 5000 /* how long (in ms) the board will listen on bootup for a DEBUG signal before starting the sketch */
+#define DEBUG_WAIT_TIME_MS 1000 /* how long (in ms) the board will listen on bootup for a DEBUG signal before starting the sketch */
 #define MAX_CMD_LENGTH 15
-
 
 void debug_start(void);
 void debug_end(void);
@@ -14,4 +14,6 @@ int parse_int_string(String int_string);
 boolean handle_command(String cmd);
 String get_command(void);
 void listen_for_debug(void);
+
+#endif TEST_H
 
