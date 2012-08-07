f7f45311b5c63e8597cee6404e5036395f6b5095
Tyler Williams <williams.tyler@gmail.com>
2012-08-06 22:06:44 -0400

    initialize byte

--- sketch/src/makey_makey/charlie.ino
+++ sketch/src/makey_makey/charlie.ino
@@ -3,7 +3,7 @@
     
 void cpled_set(CPLED led, boolean state) {
   // put any pins we don't need into a high-z state
-  for (byte i; i < sizeof(led.ignore_pins); i++) {
+  for (byte i=0; i < sizeof(led.ignore_pins); i++) {
     set_highz(led.ignore_pins[i]);
   }
   if (state) {
